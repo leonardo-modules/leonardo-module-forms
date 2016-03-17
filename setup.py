a6ec1de790da34cc093f2c6338d3a55f40654263
Michael Kutý <6du1ro.n@gmail.com>
2016-03-17 11:17:01 +0100

    Do not use custom pbr.

--- setup.py
+++ setup.py
@@ -11,6 +11,4 @@ except ImportError:
 
 setuptools.setup(
     setup_requires=['pbr'],
-    pbr=True,
-    dependency_links=[
-        'https://github.com/michaelkuty/pbr/tarball/master#egg=pbr'])
+    pbr=True)
